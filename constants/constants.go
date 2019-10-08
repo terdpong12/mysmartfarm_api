677c2857d9e018f26eecdd0d4382d47e3f60e27d
Terdpong <terdpong.sr@gmail.com>
2019-10-09 06:39:17 +0700

    Set env

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	MSFEnvironmentModeKey = "MSF_ENV_MODE"
+
+	InfluxdbUsername = "INFLUXDB_ADMIN_USER"
+	InfluxdbPassword = "INFLUXDB_ADMIN_PASSWORD"
+
+	NotifyLineToken = "NOTIFY_LINE_TOKEN"
+)
